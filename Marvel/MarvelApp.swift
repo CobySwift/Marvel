fe57ce1b4c856f1458a2b4badd5ee55855b2ef06
coby5502 <57849386+coby5502@users.noreply.github.com>
2023-07-13 13:41:33 +0900

    Initial Commit

---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by COBY_PRO on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
